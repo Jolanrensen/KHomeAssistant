cf1494f0e0294f4a3988ef1fadb52b670274f70c
Jolanrensen <j.j.r.rensen@student.tue.nl>
2020-06-27 15:47:05 +0200

    scene should be working, need testing!

--- KHomeAssistantLibrary/src/commonMain/kotlin/nl/jolanrensen/kHomeAssistant/entities/HassAttributes.kt
+++ KHomeAssistantLibrary/src/commonMain/kotlin/nl/jolanrensen/kHomeAssistant/entities/HassAttributes.kt
@@ -7,6 +7,8 @@ typealias BaseHassAttributes = HassAttributes
  * Base attributes for any entity.
  * Some attributes might not be present on an entity, which will throw an exception.
  * These attributes correspond directly to the attributes found in Home Assistant.
+ * Helper functions or getter/setters are also allowed and encouraged to be defined in the interface
+ * as long as they are implemented. Abstract attributes must correspond in name and type directly to Home Assistant.
  **/
 interface HassAttributes {
     /** Name of the entity as displayed in the UI. */
@@ -18,7 +20,7 @@ interface HassAttributes {
     /** URL used as picture for entity. */
     val entity_picture: String
 
-    /** Icon used for this enitity. Usually of the kind "mdi:icon" */
+    /** Icon used for this entity. Usually of the kind "mdi:icon" */
     val icon: String
 
     /** For switches with an assumed state two buttons are shown (turn off, turn on) instead of a switch. If assumed_state is false you will get the default switch icon. */
